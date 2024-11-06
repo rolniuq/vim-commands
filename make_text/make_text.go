f41718d1316b9d68bb7355e462b0fcfc583c7c45
rolnuiqdev <truongquynh2525@gmail.com>
2024-11-06 20:59:28 +0700

    chore: mark text

---
+++ make_text/make_text.go
@@ -0,0 +1,29 @@
+package maketext
+
+const (
+	MakeTextName = "maketext"
+)
+
+type MakeText struct{}
+
+func NewMakeText() *MakeText {
+	return &MakeText{}
+}
+
+func (s *MakeText) GetCommands() map[string]string {
+	return map[string]string{
+		"v":             "start visual mode, mark lines, then do a command (ex: yank)",
+		"V":             "start linewise visual mode",
+		"o":             "move to the end of marked area",
+		"Ctrl+v":        "start visual block mode",
+		"0":             "move to other corner of block",
+		"aw":            "mark a word",
+		"ab":            "a block with ()",
+		"at":            "a block with <> tags",
+		"ib":            "inner block with ()",
+		"iB":            "inner block with {}",
+		"it":            "inner block with <> tags",
+		"ESC or Ctrl+c": "exit visual mode",
+		"Tips":          "b or B can also use ( or { respectively",
+	}
+}
